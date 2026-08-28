4f476b8aa4bc08bdd4196e1f5d2402f6bcd9f5dd
Maxim <alo.kiosk.aarau@gmail.com>
2026-08-29 00:49:46 +0200

    Fix Shopify Admin action origin

--- shopify-app/alo-platform/react-router.config.ts
+++ shopify-app/alo-platform/react-router.config.ts
@@ -2,6 +2,6 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   allowedActionOrigins: [
-    "admin.shopify.com",
+    "https://admin.shopify.com",
   ],
 } satisfies Config;
