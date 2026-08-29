803442cd0eb372e2512cea2a0dcf67dc462459ba
Maxim <alo.kiosk.aarau@gmail.com>
2026-08-29 13:04:34 +0200

    Fix Shopify action origin

--- shopify-app/alo-platform/react-router.config.ts
+++ shopify-app/alo-platform/react-router.config.ts
@@ -3,6 +3,8 @@ import type { Config } from "@react-router/dev/config";
 export default {
   allowedActionOrigins: [
     "admin.shopify.com",
+    "alo-kiosk.myshopify.com",
     "localhost:8080",
+    "daring-acceptance-production-049e.up.railway.app",
   ],
 } satisfies Config;
